2235b9ae48e202383b1836d39bded67782cbc307
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-01 16:52:49 -0300

    :construtcion: build: Home

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
+import AmountPts from "../components/AmountPts";
+import CardActivitie from "../components/CardActivitie";
 import Layout from "../components/Layout";
+import { activities }  from '../data/data'
 
 export default function Home() {
   return (
     <div>
       <Layout>
-
+        <div>
+          {activities.map((activitie) => (
+            <CardActivitie key={activitie} activitie={activitie} />
+          ))}
+        </div>
+        <AmountPts activities={activities}/>
       </Layout>
     </div>
   )
