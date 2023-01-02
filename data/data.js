40b2ac1b066213806333be0b8c8316bed8bc6ae3
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-01 23:59:09 -0300

    :sparkles: feat: atualiza data

--- data/data.js
+++ data/data.js
@@ -1,27 +1,26 @@
 
-const project = { name: 'project', pts: 10 }
-const postLinkedin = {name: 'post linkedin', pts: 8}
+const project = { name: 'Project', pts: 10 }
+const postLinkedin = { name: 'Post linkedin', pts: 8 }
+const testLinkedin = { name: 'Teste linkedin', pts: 1 }
 
 export const activities = [
-    { year: 2023, month: 1, day: 1, type: project.name, pts: project.pts , desc: ''},
-    { year: 2023, month: 1, day: 1, type: postLinkedin.name, pts: postLinkedin.pts, desc: 'oiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii' },
-    { year: 2023, month: 1, day: 2, type: project.name, pts: project.pts , desc: ''},
-    { year: 2023, month: 1, day: 2, type:postLinkedin.name, pts:postLinkedin.pts, desc: 'oiiiiiiiiiiiiiiitrtrtree' }
+  { year: 2023, month: 1, day: 1, type: postLinkedin.name, pts: postLinkedin.pts, desc: 'Postei no linkedin MyBooks' },
+  { year: 2023, month: 1, day: 1, type: testLinkedin.name, pts: testLinkedin.pts, desc: 'Teste de JavaScript' },
 ]
 
 export const activitiesByDay = activities.reduce((days, activity) => {
-    const day = `${activity.year}-${activity.month}-${activity.day}`;
-    if (!days[day]) {
-      days[day] = [];
-    }
-    days[day].push(activity);
-    return days;
+  const day = `${activity.year}-${activity.month}-${activity.day}`;
+  if (!days[day]) {
+    days[day] = [];
+  }
+  days[day].push(activity);
+  return days;
 }, {});
 
 
 
 const today = new Date();
-const startDate = new Date('2022-12-25');
+const startDate = new Date('2023-1-1');
 
 export const activitiesByDayArray = [];
 for (let date = startDate; date <= today; date.setDate(date.getDate() + 1)) {
