a9c0653202bf737b4c7f7b9627d05ee71729d6ff
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-01 23:58:20 -0300

    :lipstick: style: estiliza card d activity

--- components/CardActivitie.jsx
+++ components/CardActivitie.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import styles from '../styles/CardActivitie.module.css'
 
 const CardActivitie = (props) => {
 
     const activitie = props.activitie
 
   return (
-        <div  className='text-black p-5'>
-          <div>{activitie.type}</div>
-          <div>Pontuação: {activitie.pts}</div>
-          <div>{activitie.desc}</div>
+        <div className={`card ${styles.card}`}>
           <div>
-              <span>{activitie.day} / </span>
-              <span>{activitie.month} / </span>
+            <div className={styles.type}>{activitie.type}</div>
+            <div className={styles.pts}>{activitie.pts} pts</div>
+            <div>{activitie.desc}</div>
+          </div>
+          <div>
+              <span>{activitie.day}/</span>
+              <span>{activitie.month}/</span>
               <span>{activitie.year}</span>
           </div>
         </div>
