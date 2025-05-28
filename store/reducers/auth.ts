977888789a6405b4a5cef088fd25468cbed7f4b1
Ruan Ubunto <ruanazeredo@gmail.com>
2025-05-28 14:44:30 -0300

    :construction: build: auth simple logic

---
+++ store/reducers/auth.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface AuthState {
+    isAuthenticated: boolean;
+}
+
+const initialState: AuthState = {
+    isAuthenticated: false
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        login: (state, action: PayloadAction<{user: string, password: string}>) => {
+            if(
+                process.env.NEXT_PUBLIC_EMAIL === action.payload.user &&
+                process.env.NEXT_PUBLIC_PASSWORD === action.payload.password
+            ){
+                state.isAuthenticated = true
+            }
+        },
+        logout: (state) => {
+            state.isAuthenticated = false
+        }
+    }
+})
+
+export const { login, logout } = authSlice.actions
+export default authSlice.reducer
