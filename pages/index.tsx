4b19d03d884054afa7833ff6b907c0bb5ca1733a
Ruan Ubunto <ruanazeredo@gmail.com>
2025-05-28 14:48:45 -0300

    :lipstick: feat: style login

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,11 +66,6 @@ export default function Home() {
 		<div className="flex h-screen">
 			<div className="bg-white m-4 flex rounded-lg flex-1 flex-col justify-center px-6 py-12 lg:px-8">
 					<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-					  <img
-						alt="Your Company"
-						src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-						className="mx-auto h-10 w-auto"
-					  />
 					  <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
 						Sign in to your account
 					  </h2>
@@ -100,11 +95,6 @@ export default function Home() {
 							<label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
 							  Password
 							</label>
-							<div className="text-sm">
-							  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-								Forgot password?
-							  </a>
-							</div>
 						  </div>
 						  <div className="mt-2">
 							<input
@@ -121,20 +111,13 @@ export default function Home() {
 			
 						<div>
 						  <button
-							onClick={() => dispatch(login({user: process.env.NEXT_PUBLIC_EMAIL, password: process.env.NEXT_PUBLIC_PASSWORD}))}
-							className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+							onClick={() => dispatch(login({email: email, password: password}))}
+							className="flex w-full justify-center rounded-md bg-gray-900 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-gray-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 						  >
 							Sign in
 						  </button>
 						</div>
 					  </form>
-			
-					  <p className="mt-10 text-center text-sm/6 text-gray-500">
-						Not a member?{' '}
-						<a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-						  Start a 14 day free trial
-						</a>
-					  </p>
 					</div>
 				  </div>
 		</div>
