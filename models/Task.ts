7aabb415846d0476890b7d28e3b8baaaaaccc24b
Ruan-M2 <ruanazeredo@gmail.com>
2025-06-01 23:07:42 -0300

    :bug: fix: add description in task schema

--- models/Task.ts
+++ models/Task.ts
@@ -3,6 +3,7 @@ import mongoose, { Schema } from "mongoose";
 const TaskSchema = new Schema({
     customId: { type: String, required: true, unique: true },
     title: { type: String, required: true },
+    description: { type: String, default: null },
     date: { type: Number },
     completed_date: { type: Number },
     completed: { type: Boolean, default: false },
