2e6c3df3800564855a7f43701bcfe16edfb417ce
Ruan-M2 <ruanazeredo@gmail.com>
2025-06-18 00:44:25 -0300

    teste

---
+++ commitlint.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "imp", "wip", "chore", "docs", "style", "refactor", "test", "ci", "release"],
+    ],
+  },
+};
