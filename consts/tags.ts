4a6295008c7b0872b9db50760b5d171f7592f29e
Ruan Ubunto <ruanazeredo@gmail.com>
2025-06-24 22:03:53 -0300

    feat: tags list

--- consts/tags.ts
+++ consts/tags.ts
@@ -1,7 +1,42 @@
 export const tags_imgs = {
-    myroutine: 'https://icon-library.com/images/ios-clock-icon/ios-clock-icon-15.jpg',
+    myroutine: 'https://i.pinimg.com/736x/dc/dd/bd/dcddbdbf83b3846787a9b5c2e2cd2ba5.jpg',
     talqui: 'https://yt3.googleusercontent.com/_my-F0L9H3027ac6nkvUnHl4fimWVNnmNC28otn_rb2va6-qnIo_Va2bXIkukmMcAWHOW27_=s900-c-k-c0x00ffffff-no-rj',
     rupert: '/rupert.png',
     uvv:'https://disciplinas.uvv.br/assets/images/uvv-brasao.png',
-    spendlyzer: '/spendlyzer.png'
+    spendlyzer: '/spendlyzer.png',
+    lifeOtherThings: 'https://png.pngtree.com/png-clipart/20220213/original/pngtree-papercut-white-cloud-icon-png-image_7266753.png'
 }
+
+export const tags_list = [
+    {
+        name: 'myroutine',
+        image: 'https://i.pinimg.com/736x/dc/dd/bd/dcddbdbf83b3846787a9b5c2e2cd2ba5.jpg',
+        color: '#000000'
+    },
+    {
+        name: 'talqui',
+        image: 'https://yt3.googleusercontent.com/_my-F0L9H3027ac6nkvUnHl4fimWVNnmNC28otn_rb2va6-qnIo_Va2bXIkukmMcAWHOW27_=s900-c-k-c0x00ffffff-no-rj',
+        color: '#007aff'
+    },
+    {
+        name: 'rupert',
+        image: '/rupert.png',
+        color: '#10B981'
+    },
+    {
+        name: 'uvv',
+        image: 'https://disciplinas.uvv.br/assets/images/uvv-brasao.png',
+        color: '#F59E0B'
+    },
+    {
+        name: 'spendlyzer',
+        image: '/spendlyzer.png',
+        color: '#111827'
+    },
+    {
+        name: 'lifeOtherThings',
+        image: 'https://png.pngtree.com/png-clipart/20220213/original/pngtree-papercut-white-cloud-icon-png-image_7266753.png',
+        color: '#10B981'
+    },
+    
+]
